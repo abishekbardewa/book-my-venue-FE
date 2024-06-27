a833409757ef690a640c01ac2dafa32279148ac8
Abishek Bardewa <abishek@distinctcloud.io>
2024-06-27 23:14:35 +0530

    Updates

--- src/components/navbar/Logo.jsx
+++ src/components/navbar/Logo.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from '/logo.png';
+
 import { Link } from 'react-router-dom';
 
 export default function Logo() {
 	return (
 		<Link to="/" className="hidden md:block cursor-pointer rounded-3xl">
-			{/* <img src={logo}  height="180" width="180" alt="Logo" /> */}
+			{/* <img src={logo} height="180" width="180" alt="Logo" /> */}
 			<div className="text-xl  font-bold  text-primary">BookMyVenue</div>
 		</Link>
 	);
